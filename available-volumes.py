7e02a58932ec6798e1e965c8648b02963b80cf59
Sandeep Dosapati <sandeep.dosapati@gmail.com>
2021-05-12 11:08:36 +0530

    updated py file to return the volumes list over the mail

--- available-volumes.py
+++ available-volumes.py
@@ -5,13 +5,13 @@ from pprint import pprint
 import os
 
 client = boto3.client('ec2', region_name= 'us-east-1')
-# create a list where the volume ids of unused volumes will be stored
-available_volumes = list()
 
 def lambda_handler(event, context):
   # call describe_volumes() method of client to get the details of all ebs volumes in given region
   # if you have large number of volumes then get the volume detail in batch by using nextToken and process accordingly
   volume_detail = client.describe_volumes()
+  # create a list where the volume ids of unused volumes will be stored
+  available_volumes = list()
 
   # process each volume in volume_detail
   if volume_detail['ResponseMetadata']['HTTPStatusCode'] == 200:
@@ -26,6 +26,7 @@ def lambda_handler(event, context):
           # the volumes which do not have 'Attachments' key and their state is 'available' is considered to be unused
           if len(each_volume['Attachments']) == 0 and each_volume['State'] == 'available':
               available_volumes.append(each_volume['VolumeId'])
+  return available_volumes
 
   # Printing the volumes
   pprint(available_volumes)
